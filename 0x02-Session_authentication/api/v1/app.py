519fcbc2b8338d5c1ff0440d2a5ab179c1579779
whalay <soliuy5@gmail.com>
2023-10-04 01:24:50 +0100

    added

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -23,20 +23,19 @@ else:
 
 @app.before_request
 def before_request():
-    """ filters requests """
-    if not auth:
-        return
+    """execute before each request
+    """
+    if auth is not None:
 
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    request.current_user = auth.current_user(request)
-    if auth.current_user(request) is None:
-        abort(403)
+        excluded_paths = ['/api/v1/status/',
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+            request.current_user = auth.current_user(request)
 
 
 
